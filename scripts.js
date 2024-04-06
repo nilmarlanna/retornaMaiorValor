468602b7d375235fcfdd248dacda40350134ea27
Nilmar Lanna <73363559+nilmarlanna@users.noreply.github.com>
2024-04-06 16:44:37 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,10 @@
+// Escrever uma função que usa 2 numeros e retorna o maior entre eles
+
+letvalorMaior = max(10,10);
+console.log(valorMaior);
+
+function max(numero1, numero2) {
+    if(numero1 > numero2)
+    return numero1;
+else return numero2;
+}
